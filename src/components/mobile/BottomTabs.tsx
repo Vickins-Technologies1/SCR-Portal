ad2b91bf26097c55114c2bfbbd841954944c9296
Vickins1 <vickinstechnologies@gmail.com>
2026-08-13 07:36:00 +0300

    Add js-cookie dependency import to mobile bottom tabs

--- src/components/mobile/BottomTabs.tsx
+++ src/components/mobile/BottomTabs.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
 import type { ReactNode } from "react";
+import Cookies from "js-cookie";
 import {
   LayoutDashboard,
   CreditCard,
